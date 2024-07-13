649294e48d34cd88fbdb5fb12d005db460160e38
Vrushank <raovrushank@gmail.com>
2024-07-13 18:12:13 +0530

    Loaded dotenv

--- flask-own/main/__init__.py
+++ flask-own/main/__init__.py
@@ -4,8 +4,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 import os
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
